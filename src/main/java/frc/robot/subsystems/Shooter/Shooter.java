08f153dcfc57b1feb0c603dc022f838f8ebf2bba
Sunk <125746063+SunkCircle88900@users.noreply.github.com>
2025-08-11 01:32:10 +0800

    set up shooter / intake

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -1,15 +1,19 @@
 package frc.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+
+import java.util.function.BooleanSupplier;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
 
 public class Shooter  extends SubsystemBase {
     private final TalonFX ShooterMotor = new TalonFX(10);
+    public final DigitalInput beamBreak = new DigitalInput(0);
 
    public Command runShooter() {
     return Commands.run(
@@ -36,6 +40,31 @@ public class Shooter  extends SubsystemBase {
 
     
 
+  }
+
+  
+  public BooleanSupplier beamBroken = () -> !beamBreak.get();
+  public BooleanSupplier beamNotBroken = () -> beamBreak.get();
+
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
   }
 }
 
