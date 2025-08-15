b675b338e72d873db01d66b81808f4e969a94dbe
Sunk <125746063+SunkCircle88900@users.noreply.github.com>
2025-08-15 16:51:56 +0800

    Updated the phoenix tuner project

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,10 +90,10 @@ public class RobotContainer {
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-       joystick.povUp().and(joystick.triangle()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-       joystick.povUp().and(joystick.square()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-       joystick.povDown().and(joystick.triangle()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-        joystick.povDown().and(joystick.square()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+       //joystick.povUp().and(joystick.triangle()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+       //joystick.povUp().and(joystick.square()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+       //joystick.povDown().and(joystick.triangle()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+       //joystick.povDown().and(joystick.square()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
 
         // reset the field-centric heading on left bumper press
         joystick.PS().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
