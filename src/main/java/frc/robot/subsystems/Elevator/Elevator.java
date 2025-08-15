e8aa90b37b9d889bc018e554ec7d59f5d3ba9c6c
ppm-main <ppmcebis@gmail.com>
2025-08-15 15:08:59 +0800

    Added comments to explain values

--- src/main/java/frc/robot/subsystems/Elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -6,23 +6,23 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.configs.Slot0Configs;
 
-public class Elevator  extends SubsystemBase {
-    final TalonFX m_talonFX = new TalonFX(4);
-
-
-
-
+public class Elevator extends SubsystemBase {
+    final TalonFX m_talonFX = new TalonFX(4); // m_talonFX defined as Elevator Motor (ID:4)
 
     public Elevator() {
-        // in init function, set slot 0 gains
-        var slot0Configs = new Slot0Configs();
-        slot0Configs.kP = 0.8; // An error of 1 rotation results in 2.4 V output
-        slot0Configs.kI = 0.002; // no output for integrated error
-        slot0Configs.kD = 0; // A velocity of 1 rps results in 0.1 V output
-        slot0Configs.kG = 0.7; // A velocity of 1 rps results in 0.1 V output
+        var slot0Configs = new Slot0Configs(); // create new Slot0Configs() objet called slot0Configs
+        slot0Configs.kP = 0.8; // Proportional Voltage Adjustment to current error
+        // E.G. at kP=0.8, 1 Rotation Error = 0.8V, 2 = 1.6 V, etc.
+        slot0Configs.kI = 0.002; // Sum Voltage Adjustment to creep over error
+        // E.G. 2 Rotation Error, kP can't fix, Voltage=kI*SUM(Error*Elaspsed Time) *Sum in respect to time*
+        slot0Configs.kD = 0; // Adjusts Voltage based on Derivative (Gradient) of movement. Prevents over/under shoot.
+        // E.G. Aim for 10 Rotations at rate of 1.2rps, as r->10, rps descreases to ensure it reaches 10 with min RPS
+        slot0Configs.kG = 0.7; // Voltage needed to overcome gravity.
+        // E.G. To get value Trial and Error, calcs possible but time consuming.
 
         m_talonFX.getConfigurator().apply(slot0Configs);
     }
+
     final PositionVoltage m_request = new PositionVoltage(0).withSlot(0);
 
     public Command goToHeight(double heightRotations) {
@@ -30,11 +30,5 @@ public class Elevator  extends SubsystemBase {
         // set position to 10 rotations
         return run(() -> m_talonFX.setControl(m_request.withPosition(heightRotations)));
     }
-
-
-    
-
-
-
 
 }
